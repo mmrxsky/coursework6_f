af55f2b90f5ef96c713d17db4abc14eaa238daef
mmrxsky <mmrxsky@gmail.com>
2024-12-03 19:19:30 +0300

    add Админка

--- mailapp/admin.py
+++ mailapp/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 
-from mailapp.models import Client
+from mailapp.models import Client, Message, NewsLetter, Attempt
 
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'comment',)
     list_filter = ('name', 'email',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('letter_subject', 'body',)
+
+
+@admin.register(NewsLetter)
+class NewsLetterAdmin(admin.ModelAdmin):
+    list_display = ('start_time', 'end_time', 'frequency', 'status',)
+    list_filter = ('status',)
+
+
+@admin.register(Attempt)
+class AttemptAdmin(admin.ModelAdmin):
+    list_display = ('time', 'status', 'server_response',)
+    list_filter = ('status',)
