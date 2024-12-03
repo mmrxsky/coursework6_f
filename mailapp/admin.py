2cb76c90ecb77a647ac1f68a62024e4da9910155
mmrxsky <mmrxsky@gmail.com>
2024-12-03 19:04:01 +0300

    add Заведение в админку

--- mailapp/admin.py
+++ mailapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailapp.models import Client
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'comment',)
+    list_filter = ('name', 'email',)
